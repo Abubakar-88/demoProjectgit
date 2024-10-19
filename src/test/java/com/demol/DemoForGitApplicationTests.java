b99e8538c3d70371171e0feaddf5c51a6b5e2b6b
Abu Bakar <siddique0153@gmail.com>
2024-10-19 20:55:47 +0600

    upload a project

---
+++ src/test/java/com/demol/DemoForGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
