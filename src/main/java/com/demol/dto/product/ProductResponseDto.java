154fe55c53af6995a8844a4e92e19d053d343f76
rezwanul91 <mohammadhaquer@gmail.com>
2024-10-19 12:57:13 -0400

    add id in resdto

--- src/main/java/com/demol/dto/product/ProductResponseDto.java
+++ src/main/java/com/demol/dto/product/ProductResponseDto.java
@@ -1,5 +1,6 @@
 package com.demol.dto.product;
 
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductResponseDto {
+    private Long id;
     private String name;
     private String price;
     private String image;
